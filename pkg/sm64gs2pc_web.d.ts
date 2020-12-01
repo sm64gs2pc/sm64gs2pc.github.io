6e80b1f5023e011176eb6e6476833dca4b45c12e
io12 <7348004+io12@users.noreply.github.com>
2020-12-01 07:59:50 +0000

    Deploying to master from master @ 3827ba55834a66ea643c14bb2577535fec3cf946 🚀

--- pkg/sm64gs2pc_web.d.ts
+++ pkg/sm64gs2pc_web.d.ts
@@ -13,7 +13,7 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1d04152e138c6f5d: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6b1d8a9baf0eee76: (a: number, b: number, c: number) => void;
   readonly __wbindgen_free: (a: number, b: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
   readonly __wbindgen_start: () => void;
