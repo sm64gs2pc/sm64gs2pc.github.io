ea75f5add8183ece49623c9135002be7c1528cfe
io12 <7348004+io12@users.noreply.github.com>
2022-08-22 19:40:28 +0000

    Deploying to master from @ sm64gs2pc/sm64gs2pc@c4a130e16fce33db0a06591efeab2dd24963a92b 🚀

--- pkg/sm64gs2pc_web.js
+++ pkg/sm64gs2pc_web.js
@@ -211,7 +211,7 @@ function makeClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_20(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h45c530a28768dd72(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h306e0fa3abb1b169(arg0, arg1, addHeapObject(arg2));
 }
 
 /**
